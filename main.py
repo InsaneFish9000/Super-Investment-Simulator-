9edaaf5156c655cc31d37b00130fb26ab566b007
PRABESH_NEPAL <prabeshnepal9000@gmail.com>
2026-01-26 10:52:42 +1100

    Error Checking Method for frontend Complete

--- main.py
+++ main.py
@@ -4,8 +4,9 @@
 
 
 # Error handling - return proper error messages when things go wrong
-import datetime as dt
+from datetime import datetime as dt
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -111,6 +112,7 @@ def validate_inputs(start_date:str,
 					recurring_contribution: Optional[float] = None, 
 					start_amount: Optional[float] = None):
 	
+	error_messages = []
 	first_date = df["Date"].iloc[0]
 	last_date = df["Date"].iloc[-1]
 	format_string = "%Y-%m-%d"
@@ -119,33 +121,29 @@ def validate_inputs(start_date:str,
 	last_date_object = dt.strptime(last_date, format_string)
 
 	if (given_date_object < first_date_object or given_date_object > last_date_object):
-		return{
-			"valid": False,
-        	"errors": ["Date is outside valid range"]
-		}
-	
+		error_messages.append("Date is outside valid range")
+		
 	if (initial_contribution != None and initial_contribution < 0 ):
-		return{
-			"valid": False,
-        	"errors": ["Initial Contribution is negative"]
-		}
+		error_messages.append("Initial Contribution is negative")
 	
 	if(recurring_contribution != None and recurring_contribution < 0):
-		return{
-			"valid": False,
-        	"errors": ["Recurring Contribution is negative"]
-		}
+		error_messages.append("Recurring Contribution is negative")
 	
 	if(start_amount != None and start_amount < 0):
-		return{
-			"valid": False,
-        	"errors": ["Start Amount is negative"]
+		error_messages.append("Start Amount is negative")
+	
+	if(error_messages == []):
+		return {
+		"valid": True,
+		"errors": []
+		}
+	if(error_messages != []):
+		return {
+		"valid": False,
+		"errors": error_messages
 		}
 	
-	return {
-    "valid": True,
-    "errors": []
-}
+
 
 
 
